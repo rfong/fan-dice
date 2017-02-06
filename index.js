0909bac0a09805d1e2b93bd9ddeb43ca03130e05
rfong <rachefo@gmail.com>
2017-02-06 03:21:23 -0800

    prettification

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = angular.module('FansApp', []);
 app.controller('FansCtrl', function($scope, $http) {
 
   $scope.dice = [];
-  $scope.diceTypes = ['relation', 'transition', 'relation'];
+  $scope.displayMode = $('body').attr('display-mode');
 
   $scope.DICE_PARAMETERS = {
     // all must be unique
@@ -22,30 +22,39 @@ app.controller('FansCtrl', function($scope, $http) {
     }
   };
 
-  $scope.shuffle = function() {
-    $scope.dice = _.map($scope.diceTypes, function(dtype) {
-      return {
+  $scope.shuffle = function(diceTypes) {
+    diceTypes = diceTypes || ['relation', 'transition', 'relation'];
+    $scope.dice = _.map(diceTypes, function(dtype) {
+      var params = {
         type: dtype,
         content: _.sample($scope.DICE_PARAMETERS[dtype]),
       };
+      if ($scope.displayMode == 'pose') {
+        params.variant = _.random(
+          1, $scope.DICE_PARAMETERS.num_poses[params.content]);
+      }
+      return params;
     });
+    console.log("shuffled;", _.pluck($scope.dice, 'content'));
     $scope.updateUrlParams();
   };
 
-  $scope.getImageUrl = function(relationName) {
-    if (! _.contains($scope.DICE_PARAMETERS.relation, relationName))
+  $scope.getImageUrl = function(die) {
+    var relation = die.content;
+    if (! _.contains($scope.DICE_PARAMETERS.relation, relation))
       return null;
-    if ($('body').attr('display-mode') == 'pose') {
-      return ('/images/relation_' + relationName +
-              _.random(1, $scope.DICE_PARAMETERS.num_poses[relationName]).toString()
-              + '.png');
+    if ($scope.displayMode == 'pose' && die.variant !== undefined) {
+      return ('/images/relation_' + relation + die.variant.toString() + '.png');
     }
-    return '/images/relation_' + relationName + '.png';
+    return '/images/relation_' + relation+ '.png';
   };
 
   $scope.updateUrlParams = function() {
     // Dump config to url params
     var dice_names = _.map($scope.dice, function(die) {
+      if ($scope.displayMode == 'pose' && die.variant !== undefined) {
+        return die.content + ':' + die.variant.toString();
+      }
       return die.content;
     });
     window.urlparams.setUrlParams({dice: dice_names.join(',')});
@@ -53,9 +62,23 @@ app.controller('FansCtrl', function($scope, $http) {
 
   $scope.initialize = function() {
     // Load configuration from permalink, or shuffle if DNE
-    var params = window.urlparams.getUrlParams();
+    try {
+      var params = window.urlparams.getUrlParams();
+    } catch (ex) {
+      $scope.shuffle();
+      return;
+    }
     if (params.dice) {
+      console.log("load", params.dice);
       $scope.dice = _.map(params.dice.split(','), function(die_name) {
+        if (_.contains(die_name, ':')) {
+          die_name = die_name.split(':');
+          return {
+            type: $scope.getDieType(die_name[0]),
+            content: die_name[0],
+            variant: die_name[1],
+          };
+        }
         return {
           type: $scope.getDieType(die_name),
           content: die_name
