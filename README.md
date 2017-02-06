b14f299a6df1e1bcebdb38da329e829a3e37b094
rfong <rachefo@gmail.com>
2017-02-06 01:57:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+fan-dice
+-----
+
+Randomly generated transition exercises for techy fan spinners!
+
+Based on Becca Becker's [dice game](https://www.facebook.com/groups/109637431191/permalink/10154145560006192/).
+
+[Diagrams by Robin McPlurry](https://www.facebook.com/groups/109637431191/permalink/10154930811256192/).
---
+++ index.js
@@ -0,0 +1,65 @@
+var app = angular.module('FansApp', []);
+
+
+app.controller('FansCtrl', function($scope, $http) {
+
+  $scope.dice = [];
+  $scope.diceTypes = ['relation', 'transition', 'relation'];
+
+  $scope.DICE_PARAMETERS = {
+    // all must be unique
+    relation: ['C', 'I', 'O', 'S', 'W', 'X'],
+    transition: ['static', 'extension', 'fold', 'spin + antispin', 'tracer',
+                 'glides + slides', 'isolation', 'stack'],
+  };
+
+  $scope.getDieType = function(die_name) {
+    for (var die_type in $scope.DICE_PARAMETERS) {
+      if (_.contains($scope.DICE_PARAMETERS[die_type], die_name)) {
+        return die_type;
+      }
+    }
+  };
+
+  $scope.shuffle = function() {
+    $scope.dice = _.map($scope.diceTypes, function(dtype) {
+      return {
+        type: dtype,
+        content: _.sample($scope.DICE_PARAMETERS[dtype]),
+      };
+    });
+    $scope.updateUrlParams();
+  };
+
+  $scope.getImageUrl = function(relationName) {
+    if (! _.contains($scope.DICE_PARAMETERS.relation, relationName))
+      return null;
+    return 'images/relation_' + relationName + '.png';
+  };
+
+  $scope.updateUrlParams = function() {
+    // Dump config to url params
+    var dice_names = _.map($scope.dice, function(die) {
+      return die.content;
+    });
+    window.urlparams.setUrlParams({dice: dice_names.join(',')});
+  };
+
+  $scope.initialize = function() {
+    // Load configuration from permalink, or shuffle if DNE
+    var params = window.urlparams.getUrlParams();
+    if (params.dice) {
+      $scope.dice = _.map(params.dice.split(','), function(die_name) {
+        return {
+          type: $scope.getDieType(die_name),
+          content: die_name
+        };
+      });
+    }
+    else {
+      $scope.shuffle();
+    }
+  };
+
+  $scope.initialize();
+});
